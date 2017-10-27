13b7e55f24c822ca1f7627f2791b87fc91d0aac5
barry <whlitiger_yp@163.com>
2017-10-27 17:23:41 +0800

    接口

--- src/main/java/com/xe/demo/api/ApiActivtiy.java
+++ src/main/java/com/xe/demo/api/ApiActivtiy.java
@@ -1,29 +1,29 @@
 package com.xe.demo.api;
 
+import com.github.pagehelper.page.PageMethod;
 import com.xe.demo.common.pojo.AjaxResult;
 import com.xe.demo.common.pojo.PageAjax;
 import com.xe.demo.common.utils.OpenIdUtil;
 import com.xe.demo.mapper.ActivityMapper;
-import com.xe.demo.model.Activity;
-import com.xe.demo.model.ActivityType;
-import com.xe.demo.model.AuthRole;
-import com.xe.demo.model.Member;
+import com.xe.demo.mapper.MemberViewActivityMapper;
+import com.xe.demo.model.*;
 import com.xe.demo.service.ActivityService;
 import com.xe.demo.service.ActivityTypeService;
+import com.xe.demo.service.MemberService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import net.sf.json.JSONObject;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tk.mybatis.mapper.entity.Condition;
 import tk.mybatis.mapper.entity.Example;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017-10-13.
@@ -40,6 +40,11 @@ public class ApiActivtiy {
 
     @Autowired
     private ActivityMapper activityMapper;
+
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MemberViewActivityMapper memberViewActivityMapper;
     /*@Authorization("需token")*/
     @ApiOperation(value="获取所有的活动类型", notes="获取所有的活动类型")
     @RequestMapping(value = "getActivtiyallType", method = RequestMethod.POST)
@@ -55,13 +60,22 @@ public class ApiActivtiy {
 /*     @Authorization("需token")*/
      @ApiOperation(value="根据活动类型获取对应的活动", notes="根据活动类型获取对应的活动")
      @RequestMapping(value = "getActivtiyByType", method = RequestMethod.POST)
-     @ApiImplicitParam(paramType="query", name = "typeid", value = "活动类型编号", required = true, dataType = "String")
+     @ApiImplicitParam(paramType="query", name = "typeid",  value = "活动类型编号", required = true, dataType = "String")
     public AjaxResult getActivtiyByType(@RequestParam String  typeid)
      {
+
+
+       //  RowBounds rowBounds = new RowBounds(pageNo, pageSize);
+         /*}
+         else {
+             RowBounds rowBounds = new RowBounds();
+         }*/
         JsonResult r = new JsonResult();
         Condition condition=new Condition(Activity.class);
-        condition.createCriteria().andCondition("activitytype = '"+typeid+"'");
+        condition.createCriteria().andCondition("activitytype = '"+typeid+"'").andCondition("state = '1'");
         condition.setOrderByClause("createtime desc");
+
+      //   PageMethod.startPage(1, 99999);
         List<Activity> list = activityMapper.selectByExample(condition);
         AjaxResult aa=new AjaxResult();
         aa.setData(list);
@@ -73,14 +87,28 @@ public class ApiActivtiy {
     @ApiOperation(value="根据活动id获取活动详情", notes="根据活动id获取活动详情")
     @RequestMapping(value = "getActivtiyById", method = RequestMethod.POST)
     @ApiImplicitParam(paramType="query", name = "id", value = "活动编号", required = true, dataType = "String")
-    public AjaxResult getActivtiyById(@RequestParam String  id)
+    public AjaxResult getActivtiyById(@RequestParam String  id,@RequestParam String  openId)
     {
+
+        Member mb=memberService.queryByID(openId);
+        if(mb!=null) {
+            MemberViewActivity memberViewActivity=new MemberViewActivity();
+            memberViewActivity.setUserid(openId);
+            memberViewActivity.setActivityid(id);
+           List<MemberViewActivity> m= memberViewActivityMapper.select(memberViewActivity);
+           if(m.size()==0) {
+
+               memberViewActivity.setId(UUID.randomUUID().toString());
+               SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+               memberViewActivity.setViewtime(df.format(new Date()));
+               memberViewActivityMapper.insert(memberViewActivity);
+           }
+
+
+        }
         JsonResult r = new JsonResult();
-    /*    Condition condition=new Condition(Activity.class);
-        condition.createCriteria().andCondition("activitytype = "+id+"");
-        condition.setOrderByClause("createtime desc");*/
         Activity activity = activityService.getActivityByid(id);
-        AjaxResult aa=new AjaxResult();
+        AjaxResult aa = new AjaxResult();
         aa.setData(activity);
         aa.setRetmsg("succ");
         return aa;
